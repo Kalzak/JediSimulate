f0031b7ade592c845f7901b0ea6e40ec80e9b843
Kalzak <kalzak.dev@protonmail.com>
2024-03-18 12:05:20 +0800

    commit

---
+++ Uniswap/PoolUtils/src/CompileTrick.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+// Use inheritance to make foundry compile our desired import contracts
+
+contract CompileTrick is IERC20 {
+    uint256 public number;
+
+    function setNumber(uint256 newNumber) public {
+        number = newNumber;
+    }
+
+    function increment() public {
+        number++;
+    }
+}
